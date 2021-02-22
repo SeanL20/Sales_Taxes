eb7744c16513b57931209e57bb3c1af5e4905d4e
Sean Lim <seanl@presentplate.com>
2021-02-22 10:48:26 +1000

    Title: Created New Function To Import Data From The Csv & Create A Product Listing Class With Product Class Within It.

--- model/sales_taxes.rb
+++ model/sales_taxes.rb
@@ -1,10 +1,24 @@
 require_relative 'product_listing'
+require 'csv'
 
 class SalesTaxes
 	attr_accessor :product_listing
 
 	def initialize
 		@product_listing = nil
+	end
+
+	def input_file_import(input_file)
+		@product_listing = ProductListing.new
+		CSV.foreach(input_file, :headers => true) do |data|
+			# Create New Product From Each Line Of The CSV.
+
+			@product_listing.add_product(data["Product"], data["Quantity"], data["Price"])
+		end
+
+		puts "TEST"
+		puts @product_listing.inspect
+		puts "TEST"
 	end
 
 end
