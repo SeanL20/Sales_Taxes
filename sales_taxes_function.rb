1a531bcea8d696220688ea3dcaaa8c9dee8ad4b6
Sean Lim <seanl@presentplate.com>
2021-02-22 10:50:57 +1000

    Title: Add Function To Create Sales Taxes Class For The Tax Calculation.

--- sales_taxes_function.rb
+++ sales_taxes_function.rb
@@ -1,6 +1,13 @@
 require_relative 'model/sales_taxes'
 
 def sales_taxes_function
+	# data input file.
+	input_file = ARGV[0]
+
+	# initailize a new sales tax class.
+	sales_taxes = SalesTaxes.new
+
+	sales_taxes.input_file_import(input_file)
 end
 
 sales_taxes_function
